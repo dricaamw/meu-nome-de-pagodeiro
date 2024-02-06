837ffaea19aa9981a2e4dd50c16571aa00b1da7c
dricaamw <drica.amw@gmail.com>
2024-02-06 12:03:27 -0300

    inicio do pagode

---
+++ js/index.js
@@ -0,0 +1,31 @@
+mes.onchange = () => {
+    switch(mes.value.toLowerCase()){
+        case "janeiro":
+            nome.innerHTML = "Rei"
+        break;
+        case "janeiro":
+            nome.innerHTML = "Rei"
+        break;case "janeiro":
+        nome.innerHTML = "Rei"
+        break;case "janeiro":
+        nome.innerHTML = "Rei"
+        break;case "janeiro":
+        nome.innerHTML = "Rei"
+        break;case "janeiro":
+        nome.innerHTML = "Rei"
+        break;case "janeiro":
+        nome.innerHTML = "Rei"
+        break;case "janeiro":
+        nome.innerHTML = "Rei"
+        break;case "janeiro":
+        nome.innerHTML = "Rei"
+        break;case "janeiro":
+        nome.innerHTML = "Rei"
+        break;case "janeiro":
+        nome.innerHTML = "Rei"
+        break;case "janeiro":
+        nome.innerHTML = "Rei"
+        break;
+
+    }
+}
